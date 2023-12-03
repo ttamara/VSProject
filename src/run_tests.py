1271800870d563a7c3c5a844ed90bc15bc2899ea
ttamara <tacasavic@gmail.com>
2023-12-03 20:05:03 +0100

    Added my unit tests

--- src/run_tests.py
+++ src/run_tests.py
@@ -2,6 +2,10 @@ import os
 import subprocess
 
 def main():
+    # Add additional unit tests
+    subprocess.run(["cp", "tests/MyUnitTest1.cpp", "../04-MrSnowman/MrSnowman_tests"]) 
+    subprocess.run(["cp", "tests/MyUnitTest2.cpp", "../04-MrSnowman/MrSnowman_tests"]) 
+
     # Compile MrSnowman
     os.chdir("../04-MrSnowman/MrSnowman")
     subprocess.run(["cp", "MrSnowman.pro", "MrSnowman.pro.bak"])
@@ -19,6 +23,8 @@ def main():
     subprocess.run(["sh", "-c", 'echo "QMAKE_CXXFLAGS += --coverage" >> MrSnowman_tests.pro'])
     subprocess.run(["sh", "-c", 'echo "QMAKE_LFLAGS += --coverage" >> MrSnowman_tests.pro'])
     subprocess.run(["sh", "-c", 'echo "CONFIG -= app_bundle" >> MrSnowman_tests.pro'])
+    subprocess.run(["sh", "-c", 'echo "SOURCES += MyUnitTest1.cpp" >> MrSnowman_tests.pro'])
+    subprocess.run(["sh", "-c", 'echo "SOURCES += MyUnitTest2.cpp" >> MrSnowman_tests.pro'])
     subprocess.run(["make", "clean"])
     subprocess.run(["qmake"])
     subprocess.run(["make"])
@@ -29,6 +35,10 @@ def main():
     # Generate code coverage report
     subprocess.run(["geninfo", "--output-filename", "coverage.info", "--ignore-errors", "inconsistent,gcov", "."])
     subprocess.run(["genhtml", "coverage.info", "--ignore-errors", "inconsistent,unmapped", "--output-directory", "../../src/coverage-html"])
+
+    # Remove added unit tests
+    subprocess.run(["rm", "MyUnitTest1.cpp"])
+    subprocess.run(["rm", "MyUnitTest2.cpp"])
 
 if __name__ == "__main__":
     main()
